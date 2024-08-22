b923b463c21e85b67e644e943fe1a0688c855cc9
adarsh <jhaa50872@gmail.com>
2024-08-22 23:25:46 +0530

    first commit

---
+++ firstViteProject/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  console.log('hello react');
+  
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  ) 
+}
+
+export default App
