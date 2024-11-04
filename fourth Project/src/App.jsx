2560c675d689f98b08fa5f1bba0e0000b711f403
adarsh <jhaa50872@gmail.com>
2024-11-04 19:50:17 +0530

    applied use effect hook

--- fourth Project/src/App.jsx
+++ fourth Project/src/App.jsx
@@ -1,44 +1,50 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react'
  
 import './App.css'
 
 function App() {
   const [length, setlength] = useState(9)
-  const [password, setPassword] = useState("")
+  const [password, setPassword] = useState('')
   const [charAllowed, setCharAllowed] = useState(false)
   const [numAllowed, setNumAllowed] = useState(false)
 
   
 
   const passwordGenerator = useCallback(() =>{
-    let pass
+    let pass =''
     let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     
     if (numAllowed) string += '0123456789'
     if (charAllowed) string += "!@#$%^&*()[]{};:,.<>/?\|+=-_`~"
     
     for (let i = 0; i <= length; i++) {
-      const randomValue = Math.floor(Math.random() * length) + 1
-      console.log(randomValue);
+      const randomValue = Math.floor(Math.random() * string.length + 1)
 
-       pass = string.charAt(randomValue)
+      pass += string.charAt(randomValue)
+      console.log(pass);
+
     }
 
-  }, [length, charAllowed, numAllowed])
+    setPassword(pass)
 
+  }, [length, charAllowed, numAllowed, setPassword])
+
+  let effect = useEffect( () => passwordGenerator(), [length, charAllowed, numAllowed])
   return (
     <>
-      <h1 className='text-white rounded-2xl px-5 py-2 bg-purple-600 text-3xl font-bold'> password generator</h1>
+      <h1 className='text-yellow-500 rounded-md my-4 pb-2 px-5 text-3xl font-bold'> password generator</h1>
 
-      <div className='text-white h-48  px-2 py-10 border rounded-2xl'>
-        <input className='outline-none border-none py-1 my-6 rounded-l-md' type='text' readOnly></input>
+      <div className='w-6/12 bg-yellow-600 px-2 py-4 border rounded-md'>
+        <input className='ml-16 outline-none border-2 border-gray-700 bg-transparent text-black py-1 px-2 my-6 rounded-l-md ' readOnly type='text' value = {password}/>
         <button className='text-white rounded-r-md py-1 px-3 bg-slate-500'>copy</button>
         <br/>
 
-        <input className='mx-2 ' type='range' min={0}  max={100}/>
-        <span>length ( {length} )</span>
-        <input className='mx-2 w-5 h-5' type='checkbox'/><label htmlFor="">number</label>
-        <input className='mx-2 h-5 w-5' type='checkbox'/><label htmlFor="">characters</label>
+        <input className='mx-2' value={length} type='range' min={6}  max={20} onChange={(e) =>{
+          setlength(e.target.value)
+        }}/>
+        <label>length:{length} </label>
+        <input className='mx-2 w-5 h-5' checked = {numAllowed} type='checkbox' onChange={() => setNumAllowed(prev => !prev)}/><label htmlFor="">number</label>
+        <input className='mx-2 h-5 w-5' checked={charAllowed} type='checkbox' onChange={() => setCharAllowed(prev => !prev)} /><label htmlFor="">characters</label>
       </div>
       
     </>
