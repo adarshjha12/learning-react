9447d7012d829333f0ad9082de45a0fae782f404
adarsh <jhaa50872@gmail.com>
2024-11-05 22:46:19 +0530

    completed

--- fourth Project/src/App.jsx
+++ fourth Project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
  
 import './App.css'
 
@@ -7,8 +7,20 @@ function App() {
   const [password, setPassword] = useState('')
   const [charAllowed, setCharAllowed] = useState(false)
   const [numAllowed, setNumAllowed] = useState(false)
+  const [bgColor, setBgcolor] = useState('red')
+  const [buttonText, setButtonText] = useState('copy')
+  const refToInput = useRef(null)
 
+    const clickHandler = function() {
+      setBgcolor('green')
+      setButtonText('copied')
+
+    }
   
+    let copyPassword = useCallback( () =>{
+      refToInput.current?.select()
+      window.navigator.clipboard.writeText(password)
+    }, [password])
 
   const passwordGenerator = useCallback(() =>{
     let pass =''
@@ -17,9 +29,9 @@ function App() {
     if (numAllowed) string += '0123456789'
     if (charAllowed) string += "!@#$%^&*()[]{};:,.<>/?\|+=-_`~"
     
-    for (let i = 0; i <= length; i++) {
+    for (let i = 0; i < length; i++) {
       const randomValue = Math.floor(Math.random() * string.length + 1)
-
+ 
       pass += string.charAt(randomValue)
       console.log(pass);
 
@@ -27,24 +39,38 @@ function App() {
 
     setPassword(pass)
 
-  }, [length, charAllowed, numAllowed, setPassword])
+  }, [length, charAllowed, numAllowed])
 
-  let effect = useEffect( () => passwordGenerator(), [length, charAllowed, numAllowed])
+  useEffect( () => passwordGenerator(), [length, charAllowed, numAllowed])
+  
   return (
     <>
       <h1 className='text-yellow-500 rounded-md my-4 pb-2 px-5 text-3xl font-bold'> password generator</h1>
 
       <div className='w-6/12 bg-yellow-600 px-2 py-4 border rounded-md'>
-        <input className='ml-16 outline-none border-2 border-gray-700 bg-transparent text-black py-1 px-2 my-6 rounded-l-md ' readOnly type='text' value = {password}/>
-        <button className='text-white rounded-r-md py-1 px-3 bg-slate-500'>copy</button>
+        <input className='mr-1 ml-16 outline-none border-2 border-gray-700 bg-transparent text-black py-1 px-2 my-6 rounded-md ' readOnly type='text' ref={refToInput} value = {password}/>
+
+        <button onClick={() => {clickHandler(); copyPassword()}} style={{backgroundColor: bgColor}} className='text-white rounded-md py-1 px-3 hover:bg-slate-950'>{buttonText}</button>
         <br/>
 
         <input className='mx-2' value={length} type='range' min={6}  max={20} onChange={(e) =>{
           setlength(e.target.value)
+          setBgcolor('red')
+          setButtonText('copy')
         }}/>
         <label>length:{length} </label>
-        <input className='mx-2 w-5 h-5' checked = {numAllowed} type='checkbox' onChange={() => setNumAllowed(prev => !prev)}/><label htmlFor="">number</label>
-        <input className='mx-2 h-5 w-5' checked={charAllowed} type='checkbox' onChange={() => setCharAllowed(prev => !prev)} /><label htmlFor="">characters</label>
+        <input className='mx-2 w-5 h-5' checked = {numAllowed} type='checkbox' onChange={() => {
+
+        setNumAllowed(prev => !prev)
+        setBgcolor('red')
+        setButtonText('copy')
+
+        }}/><label htmlFor="">number</label>
+        <input className='mx-2 h-5 w-5' checked={charAllowed} type='checkbox' onChange={() => {
+          setCharAllowed(prev => !prev)
+          setBgcolor('red')
+          setButtonText('copy')
+          }} /><label htmlFor="">characters</label>
       </div>
       
     </>
