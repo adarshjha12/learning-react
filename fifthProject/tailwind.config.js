6bdc23c20e5cef542c5f51978787240b280d7232
adarsh <jhaa50872@gmail.com>
2024-11-08 18:47:28 +0530

    initializesd new project

--- fifthProject/tailwind.config.js
+++ fifthProject/tailwind.config.js
@@ -5,8 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+     'custom-blue': '0 10px 16px rgba(000, 000, 000)',
+  },
+    },
   },
   plugins: [],
 }
+
+// tailwind.config.js
+// module.exports = {
+//   theme: {
+//     extend: {
+//       boxShadow: {
+//         'custom-blue': '0 4px 6px rgba(59, 130, 246, 0.5)', // Example blue shadow
+//         'custom-red': '0 4px 6px rgba(239, 68, 68, 0.5)',    // Example red shadow
+//         'custom-green': '0 4px 6px rgba(16, 185, 129, 0.5)', // Example green shadow
+//       },
+//     },
+//   },
+// }
 
