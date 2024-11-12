d96d2e31a5036d4f5b46f6abbfc0a211c2d893ab
adarsh <jhaa50872@gmail.com>
2024-11-12 23:08:01 +0530

    done

--- practice currency converter/src/App.jsx
+++ practice currency converter/src/App.jsx
@@ -3,18 +3,61 @@ import { useState } from 'react'
 import './App.css'
 import FetchCurrency from './hooks/fetchData'
 import InputComponent from './components/inputComponent';
+
 function App() {
 
-  console.log(FetchCurrency('inr'));
-  
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState('usd')
+  const [to, setTo] = useState('inr')
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = FetchCurrency(from)
+
+  let options = Object.keys(currencyInfo)
+
+
+  const convert = () =>{
+    setConvertedAmount( amount * currencyInfo[to])
+  }
+
+  const swap = () =>{
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
   return (
     <>
-     <div>
-        <div><img src="https://images.pexels.com/photos/5519470/pexels-photo-5519470.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" /></div>
-        <div>
-          <form action="">
-              <InputComponent></InputComponent>
-              
+     <div className='text-black grid grid-cols-[1fr,2fr] gap-4 '>
+        <div className=''> <img src="https://images.pexels.com/photos/5519470/pexels-photo-5519470.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" /></div>
+        <div style={{backgroundImage: "url('https://images.pexels.com/photos/3734845/pexels-photo-3734845.jpeg?auto=compress&cs=tinysrgb&w=600')",
+        backgroundSize: 'cover'}} className='border-2 bg-gradient-to-t from-black via-transparent to-transparent relative rounded-md flex justify-center items-center'>
+          <form onSubmit={(e) => {
+            e.preventDefault()
+            convert()
+
+          }} action="" className='py-6 bg-black/50'>
+              <InputComponent
+              label='from'
+              amount={amount}
+              onAmountChange={(amount) => setAmount(amount)}
+              onCurrencyChange={(currency) => setFrom(currency)}
+              currencyOptions={options}
+              selectCurrency={from}
+              >
+
+              </InputComponent>
+              <button className='bg-blue-600 border-2 border-white text-white rounded-md px-4 py-1 font-extrabold text-xl' onClick={swap}>swap</button>
+              <InputComponent
+              label='to'
+              amount={convertedAmount}
+              onCurrencyChange={(currency) => setTo(currency)}
+              currencyOptions={options}
+              selectCurrency={to}
+              ></InputComponent>
+              <button className='bg-violet-600 border-2 border-white text-white rounded-md px-4 py-1 font-extrabold text-xl'>convert usd to inr</button>
+
           </form>
 
         </div>
--- practice currency converter/src/components/inputComponent.jsx
+++ practice currency converter/src/components/inputComponent.jsx
@@ -1,9 +1,33 @@
 
 import React from 'react'
 
-function InputComponent() {
+function InputComponent({
+    label,
+    amount,
+    onAmountChange,
+    selectCurrency = 'inr',
+    onCurrencyChange,
+    currencyOptions = []
+
+}) {
+
+
   return (
-    <div className='text-white'>inputComponent</div>
+    <div className='text-white flex gap-12 px-6 py-4'>
+        <div className='flex flex-col'>
+            <label htmlFor="input">{label}</label>
+            <input type="number" name="input" value={amount} id="input" className='text-white bg-transparent border-2 rounded-md outline-none px-3' onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}/>
+        </div>
+        <div className='flex flex-col'>
+            <label htmlFor="currencyBox">currency type</label>
+            <select name="currencyBox" id="currencyBox" value={selectCurrency} className='text-black' onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}>
+
+                {currencyOptions.map( (currency) => (
+                    <option key={currency} value={currency}>{currency}</option>
+                ))}
+            </select>
+        </div>
+    </div>
   )
 }
 
